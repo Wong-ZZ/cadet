9ce5d022a70c8f0723d61f94bd319eba535b535d
ScrubWzz <wongzhengzhi@gmail.com>
2020-03-28 17:31:12 +0800

    Modified json returned by GET user/groups route

--- lib/cadet/course/groups.ex
+++ lib/cadet/course/groups.ex
@@ -6,6 +6,10 @@ defmodule Cadet.Course.Groups do
   
   def get_group_avengers() do
     Repo.all(Group)
-    |> Enum.reduce(%{}, fn group, acc -> Map.put(acc, group.name, get_user(group.leader_id).name) end)
+    |> Enum.reduce(%{}, fn group, acc -> Map.put(acc, group.name, map_group_info(group))end)
+  end
+
+  defp map_group_info(group) do
+    %{"name" => get_user(group.leader_id).name, "id" => group.id}
   end
 end
